a3a118388f5522d7d44a7db9b91012b501726879
khomma0312 <ryukkunbob@gmail.com>
2021-05-30 12:29:30 +0900

    item.blade.phpを削除

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -3,11 +3,3 @@
 @section('error')
 	@include('components.error', ['errors' => $errors])
 @endsection
-
-@section('items')
-
-@foreach ($items as $item)
-	@include('components.item', [ 'item' => $item ])
-@endforeach
-
-@endsection
